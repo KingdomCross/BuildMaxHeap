a57bcc674d8e699f35811fd68fa0eda1599f697c
Jessica G. Banuelos <banuelos.jg@gmail.com>
2020-10-18 13:09:36 -0700

    BuildMaxHeapWeather.java

--- src/JBMHW.java
+++ src/JBMHW.java
@@ -1,2 +1,25 @@
+import java.io.*;
+
 public class JBMHW {
+
+
+        public static void main(String[] args) {
+
+            StringBuffer sb = new StringBuffer();
+            for(String s: args) {
+                sb.append(s + " ");
+            }
+            String commandPromptString = sb.toString();
+    // once I put the arg into a string, I will parse the data and allocate it in the right variables
+            System.out.println(commandPromptString);
+
+
+            String API_KEY = "aeb810a46ffabcd067cd61c6ac66c5e1";
+            String Location = null;
+
+
+
+        }
+
+
 }
