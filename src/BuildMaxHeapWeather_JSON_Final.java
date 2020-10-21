71da4575e95ce1617120acc2340e1f113ceeead7
Jessica G. Banuelos <banuelos.jg@gmail.com>
2020-10-21 10:36:05 -0700

    BuildMaxHeapWeather.java

--- src/BuildMaxHeapWeather_JSON_Final.java
+++ src/BuildMaxHeapWeather_JSON_Final.java
@@ -1,10 +1,11 @@
-import org.json.simple.JSONObject;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+
+import org.json.simple.JSONObject;
 
 public class BuildMaxHeapWeather_JSON_Final {
     public static void main(String[] args) {
@@ -45,7 +46,10 @@ public class BuildMaxHeapWeather_JSON_Final {
             buildMaxHeap(array);
             printArray(array);
             System.out.println("\n\nHeight: " + (int) (Math.log(array.length) / Math.log(2)));
-        } catch (IOException | JSONException e) {
+        } catch (MalformedURLException mURLe){
+            mURLe.printStackTrace();
+        }
+        catch(IOException e) {
             e.printStackTrace();
         }
     }
