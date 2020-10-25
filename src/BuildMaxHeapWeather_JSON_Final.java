90a5962b744d9a814232bc12ab1244e50b411a36
Jessica G. Banuelos <banuelos.jg@gmail.com>
2020-10-24 20:33:11 -0700

    BuildMaxHeapWeather.java

--- src/BuildMaxHeapWeather_JSON_Final.java
+++ src/BuildMaxHeapWeather_JSON_Final.java
@@ -1,42 +1,47 @@
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
-import org.json.simple.JSONObject;
-
-public class BuildMaxHeapWeather_JSON_Final {
-    public static void main(String[] args) {
+public class BuildMaxHeapWeather_JSON_Final
+{
+    public static void main(String[] args)
+    {
         String API_KEY = "aeb810a46ffabcd067cd61c6ac66c5e1";
-        String location = "long beach,ca,usa";
+        String location = args[0];
         String units = "imperial";
 
         String urlString = "http://api.openweathermap.org/data/2.5/weather?appid=" + API_KEY + "&q=" + location + "&units=" + units;
-        try {
+        try
+        {
             URL urlObj = new URL(urlString);
             HttpURLConnection urlCon = (HttpURLConnection) urlObj.openConnection();
 
             BufferedReader in = new BufferedReader(new InputStreamReader(urlCon.getInputStream()));
             String inputLine;
             StringBuffer response = new StringBuffer();
-            while ((inputLine = in.readLine()) != null) {
+            while ((inputLine = in.readLine()) != null)
+            {
                 response.append(inputLine);
             }
             in.close();
 
             String json = response.toString();
             JSONObject obj = new JSONObject(json);
-            int temp = (int) (Math.floor(obj.getJSONObject("main").getDouble("temp")));
+            int temp = (int)(Math.floor(obj.getJSONObject("main").getDouble("temp")));
 
             int[] array = new int[temp];
             int min = 0;
             int max = temp;
             int randomInt = 0;
 
-            for (int i = 0; i < array.length; i++) {
-                randomInt = (int) (Math.random() * (max - min + 1) + min);
+            for (int i = 0; i < array.length; i++)
+            {
+                randomInt = (int)(Math.random() * (max - min + 1) + min);
                 array[i] = randomInt;
             }
             System.out.println(temp + " randomly generated integers:");
@@ -45,42 +50,51 @@ public class BuildMaxHeapWeather_JSON_Final {
             System.out.println("\n\nMax Heap Array:");
             buildMaxHeap(array);
             printArray(array);
-            System.out.println("\n\nHeight: " + (int) (Math.log(array.length) / Math.log(2)));
-        } catch (MalformedURLException mURLe){
-            mURLe.printStackTrace();
+            System.out.println("\n\nHeight: " + (int)(Math.log(array.length) / Math.log(2)));
         }
-        catch(IOException e) {
+        catch (IOException | JSONException e)
+        {
             e.printStackTrace();
         }
     }
 
-    public static void printArray(int[] array) {
-        for (int i = 0; i < array.length; i++) {
+    public static void printArray(int[] array)
+    {
+        for (int i = 0; i < array.length; i++)
+        {
             System.out.print(array[i] + " ");
         }
     }
 
-    public static void buildMaxHeap(int[] array) {
+    public static void buildMaxHeap(int[] array)
+    {
         int heapSize = array.length;
-        for (int i = (heapSize / 2) - 1; i >= 0; i--) {
+        for (int i = (heapSize / 2) - 1; i >= 0; i--)
+        {
             maxHeapify(array, i);
         }
     }
 
-    public static void maxHeapify(int[] array, int currentIndex) {
+    public static void maxHeapify(int[] array, int currentIndex)
+    {
         int parentIndex; // parent
         int leftIndex = (2 * currentIndex) + 1; // 2 * currentIndex yields an even node index for left child
         int rightIndex = (2 * currentIndex + 1) + 1; // 2 * currentIndex + 1 yields an odd node index right child
 
-        if (leftIndex <= array.length - 1 && array[leftIndex] > array[currentIndex]) {
+        if (leftIndex <= array.length - 1 && array[leftIndex] > array[currentIndex])
+        {
             parentIndex = leftIndex;
-        } else {
+        }
+        else
+        {
             parentIndex = currentIndex;
         }
-        if (rightIndex <= array.length - 1 && array[rightIndex] > array[parentIndex]) {
+        if (rightIndex <= array.length - 1 && array[rightIndex] > array[parentIndex])
+        {
             parentIndex = rightIndex;
         }
-        if (parentIndex != currentIndex) {
+        if (parentIndex != currentIndex)
+        {
             int temp = array[currentIndex];
             array[currentIndex] = array[parentIndex];
             array[parentIndex] = temp;
---
+++ src/JBMHW.java
@@ -0,0 +1,22 @@
+
+
+public class JBMHW {
+
+
+    public static void main(String[] args) {
+
+
+           System.out.println(args[0]);
+
+        // once I put the arg into a string, I will parse the data and allocate it in the right variables
+
+
+        String API_KEY = "aeb810a46ffabcd067cd61c6ac66c5e1";
+        String Location = null;
+
+
+
+    }
+
+
+}
